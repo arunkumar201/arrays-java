7199c007b41a328cb56de0112e2e75f4ad7fdabe
Arun Kumar <arunprajapati9825@gmail.com>
2024-02-12 07:37:15 +0530

    added lc647 brute optimal sol

--- src/leetcode/lc_647.java
+++ src/leetcode/lc_647.java
@@ -35,11 +35,41 @@ public class lc_647 {
 
     }
 
+    public static int countPalindrome(String s, int low, int high) {
+        int res = 0;
+        int len = s.length();
+        while (low >= 0 && high < len) {
+            if (s.charAt(low) == s.charAt(high)) {
+                res++;
+                low--;
+                high++;
+            } else {
+                break;
+            }
+        }
+        return res;
+    }
+
+    public static int countSubstrings(String s) {
+        int res = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            int high = i;
+            //for odd length palindromes
+            res += countPalindrome(s, i, high);//O(N)
+            high = i + 1;
+            //for even length palindromes
+            res += countPalindrome(s, i, high); //O(N)
+        }
+        //overall time Complexity O(N^2)+O(N^2)=2*O(N^2)=~O(N^2)
+        return res;
+    }
+
     public static void main(String[] args) {
 
-        String s = "aaa";
+        String s = "abccbc";
 
-        int res = countSubstrings_BruteForce(s);
+        int res = countSubstrings(s);
         System.out.println("result is " + res);
 
     }
