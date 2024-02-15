bdb88ab8adffd659995046f9338a93a0d923b133
Arun Kumar <arunprajapati9825@gmail.com>
2024-02-15 06:48:51 +0530

    completed lc 2743 and added new problem from int-bit

--- src/leetcode/lc_2743.java
+++ src/leetcode/lc_2743.java
@@ -1,16 +1,50 @@
 package leetcode;
 
+
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class lc_2743 {
-    //pending problem
-    public static int numberOfSpecialSubstrings(String s) {
-        HashSet<Character> set = new HashSet<>();
-        int maxStr = 0;
-        for (int i = 0; i < s.length(); i++) {
-            maxStr += 1;
+    ///new problem
+//    https://www.interviewbit.com/problems/colorful-number/
+    public static int colorful(int num) {
+        if (num < 10) {
+            return 1;
         }
-        return maxStr;
+
+        HashSet<Integer> set = new HashSet<>();
+        String s = String.valueOf(num);
+        for (int i = 0; i < s.length(); i++) {
+            int prod = 1;
+            for (int j = i; j < s.length(); j++) {
+                System.out.println(s.charAt(j) - '0');
+                prod = prod * (s.charAt(j) - '0');
+                if (set.contains(prod)) {
+                    return 0;
+                }
+                set.add(prod);
+            }
+
+        }
+        return 1;
+    }
+
+    public static int numberOfSpecialSubstrings(String s) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+        int ans = 0;
+        int j = 0;
+        int i = 0;
+        while (j < s.length()) {
+            mp.put(s.charAt(j), mp.getOrDefault(s.charAt(j), 0) + 1);
+            while (mp.get(s.charAt(j)) > 1) {
+                mp.put(s.charAt(i), mp.get(s.charAt(i)) - 1);
+                i++;
+            }
+            ans += j - i + 1;
+            j++;
+
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
@@ -18,5 +52,8 @@ public class lc_2743 {
         String s = "abcd";
         int res = numberOfSpecialSubstrings(s);
         System.out.println("result is " + res);
+        int a = 23;
+        int res1 = colorful(a);
+        System.out.println("result is " + res1);
     }
 }
