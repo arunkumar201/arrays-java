3722e9e0f025a434ea4ed2ba9df1c06c75734ab1
Arun Kumar <arunprajapati9825@gmail.com>
2024-03-17 09:45:57 +0530

    updated 238

--- src/leetcode/lc_238.java
+++ src/leetcode/lc_238.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 public class lc_238 {
 
     public int[] productExceptSelf_BruteForce(int[] nums) {
+        //Time Complexity: O(N^2)
+        //Space Complexity: O(1)
         int[] result = new int[nums.length];
         int n = nums.length;
         int total = 1;
@@ -21,6 +23,9 @@ public class lc_238 {
     }
 
     public int[] productExceptSelf_usingDivisor(int[] nums) {
+        //Time Complexity: O(N)
+        //Space Complexity: O(1)
+        //but this Question is required to not use divisor method
         int n = nums.length;
         int[] result = new int[n];
         int total_product_without_zero = 1;
@@ -51,11 +56,44 @@ public class lc_238 {
         }
         return result;
     }
+    //Without using divisor method
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int[] result=new int[n];
+        int[] postfix_product=new int[n];
+        int[] prefix_product=new int[n];
+      for(int i=n-1;i>=0;i--){
+         if(i==n-1){
+             postfix_product[i]= nums[i];
+         }else{
+         postfix_product[i]=postfix_product[i+1]*nums[i];
+         }
+     }
+
+      for(int j=0;j<n;j++){
+          if(j==0){
+              prefix_product[j]=nums[j];
+          }else {
+              prefix_product[j]=prefix_product[j-1]*nums[j];
+          }
+      }
+
+      for(int k=0;k<n;k++){
+          if(k==0){
+              result[k]=postfix_product[k+1];
+          }else if(k==n-1){
+              result[k]=prefix_product[k-1];
+          }else{
+              result[k]=prefix_product[k-1]*postfix_product[k+1];
+          }
+      }
+     return  result;
+    }
 
     public static void main(String[] args) {
         int[] nums = new int[]{1,2,3,4};
 
-        int[] res = new lc_238().productExceptSelf_usingDivisor(nums);
+        int[] res = new lc_238().productExceptSelf(nums);
         System.out.println(Arrays.toString(res));
     }
 }
