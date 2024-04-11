1a9a2064365abc2297e3a2d869729dd7768c4644
Arun Kumar <arunprajapati9825@gmail.com>
2024-04-12 00:08:05 +0530

    added lc 402

---
+++ src/leetcode/lc_402.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class lc_402 {
+    public String removeKdigits(String num, int k) {
+        // base case
+        if (k == num.length())
+            return "0";
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < num.length(); i++) {
+            char c = num.charAt(i);
+            while (!stack.isEmpty() && c < stack.peek() && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(c);
+        }
+        System.out.println(stack);
+        while (k > 0 && !stack.isEmpty()) {
+            stack.pop();
+            k--;
+        }
+        StringBuilder res = new StringBuilder();
+        while (!stack.isEmpty()) {
+            res.append(stack.pop());
+        }
+        while (res.length() > 1 && res.charAt(res.length() - 1) == '0') {
+            res.deleteCharAt(res.length() - 1);
+        }
+        return res.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        // num = "1432219", k = 3
+        String num = "112";
+        int k = 1;
+        int res = Integer.parseInt(new lc_402().removeKdigits(num, k));
+        System.out.println(res);
+
+    }
+}
