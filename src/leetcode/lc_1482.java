0fa284f3641da51bb374ff7f8008c8e9dfbb586b
Arun Kumar <arunprajapati9825@gmail.com>
2024-06-20 22:59:55 +0530

    Added lc 1482

--- src/leetcode/lc_1482.java
+++ src/leetcode/lc_1482.java
@@ -14,24 +14,14 @@ public class lc_1482 {
         //calculate the max Day from the bloomDay
         for (int day : bloomDay) {
             maxDay = Math.max(maxDay, day);
-            minDay = Math.min(min, day);
+            minDay = Math.min(minDay, day);
         }
         //start from day 1 to max
         for (int day = minDay; day <= maxDay; day++) {
-            int bouquets = 0;
-            int flowers = 0;
-            for (int i : bloomDay) {
-                if (i <= day) {
-                    flowers++;
-                    if (flowers == k) {
-                        bouquets++;
-                        flowers = 0;
-                    }
-                } else {
-                    flowers = 0;
-                }
-            }
-            if (bouquets >= m) {
+            int minBouquetsForDaya = PossibleBouquets(day, bloomDay, k);
+            System.out.println(minBouquetsForDaya);
+
+            if (minBouquetsForDaya >= m) {
                 return day;
             }
         }
@@ -50,6 +40,7 @@ public class lc_1482 {
         int right = maxDay;
 
         while (left <= right) {
+
             int mid = left + (right - left) / 2;
             int bouquets = 0;
             int flowers = 0;
@@ -76,10 +67,26 @@ public class lc_1482 {
         return left;
     }
 
+    private static int PossibleBouquets(int day, int[] bloomDay, int k) {
+        int n = bloomDay.length;
+        int ans = 0;
+        int count = 0;
+        for (int j : bloomDay) {
+            if (j <= day) {
+                count++;
+            } else {
+                ans += count / k;
+                count = 0;
+            }
+        }
+        ans+=count/k;
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] bloomDay = {1, 10, 3, 10, 2};
         int m = 3;
-        int k = 2;
+        int k = 1;
         System.out.println("Brute Force Solution:- " + minDays_BruteForce(bloomDay, m, k));
         System.out.println("Optimal Solution:- " + minDays(bloomDay, m, k));
     }
