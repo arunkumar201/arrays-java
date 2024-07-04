17b755924dd9b9cd522cb9ffb1548c50268cbae2
Arun Kumar <arunprajapati9825@gmail.com>
2024-07-04 07:26:59 +0530

    imrv sol for lc 1509

--- src/leetcode/lc_1509.java
+++ src/leetcode/lc_1509.java
@@ -6,19 +6,21 @@ public class lc_1509 {
 
     public int minDifference(int[] nums) {
         int n = nums.length;
-        if (n <= 3) {
+        if (n <= 4) {
             return 0;
         }
         Arrays.sort(nums);
 
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i <= 3; i++) {
-            System.out.println(nums[n - 4 + i] + " " + nums[i]);
-            min = Math.min(min, nums[n - 4 + i] - nums[i]);
-        }
+
+        min = Math.min(min, nums[n - 2] - nums[2]); // case:1 - 2 smallest and 1 largest
+        min = Math.min(min, nums[n - 3] - nums[1]); // case:2- 2 largest and 1 smallest
+        min = Math.min(min, nums[n - 4] - nums[0]);// case 3: - 0 smallest and 3 largest
+        min = Math.min(min, nums[n - 1] - nums[3]);// case 4: - 3 smallest and 0 largest
 
         return min;
     }
+
 
     public static void main(String[] args) {
         lc_1509 obj = new lc_1509();
