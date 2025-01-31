23890d389090bd5664c90023de7449b98b646353
Arun Kumar <arunprajapati9825@gmail.com>
2025-01-31 07:31:17 +0530

    added lc 3217

--- src/leetcode/ListNode.java
+++ src/leetcode/ListNode.java
@@ -7,4 +7,13 @@ public class ListNode {
     public ListNode(int val) {
         this.val = val;
     }
+    public static void  DisplayList(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        while (head != null) {
+            sb.append(head.val).append(" -> ");
+            head = head.next;
+        }
+        sb.append("null");
+        System.out.println(sb.toString());
+    }
 }
