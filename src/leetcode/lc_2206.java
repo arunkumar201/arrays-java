1c8040138c748666afdb421b0aac9a9e29bd1c0a
Arun Kumar <arunprajapati9825@gmail.com>
2025-03-17 08:22:00 +0530

    add lc 1684

--- src/leetcode/lc_2206.java
+++ src/leetcode/lc_2206.java
@@ -1,2 +1,29 @@
-package leetcode;public class lc_2206 {
+package leetcode;
+
+public class lc_2206 {
+	
+	public static void main(String[] args) {
+		//Input: nums = [3,2,3,2,2,2]
+		//Output: true
+		
+		int[] nums = {1, 2, 3, 4, 4, 3};
+		System.out.println(new lc_2206().divideArray(nums)); // Output: true
+		
+	}
+	
+	public boolean divideArray(int[] nums) {
+		int[] count = new int[501];
+		int n = nums.length;
+		for (int num : nums) {
+			count[num]++;
+		}
+		
+		for (int i = 0; i < count.length; i++) {
+			if (count[i] != 0 && count[i] % 2 != 0) {
+				return false;  // if any element is odd, then return false
+			}
+		}
+		// if all elements are even, then return true
+		return true;
+	}
 }
