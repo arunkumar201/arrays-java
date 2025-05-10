783c87b86217d49d4e9245ffaebf66bb43ebc7c9
Arun Kumar <arunprajapati9825@gmail.com>
2025-05-10 20:39:37 +0530

    updated lc 231

--- src/leetcode/lc_231.java
+++ src/leetcode/lc_231.java
@@ -1,20 +1,31 @@
 package leetcode;
 
 public class lc_231 {
-
-    public static boolean isPowerOfTwo(int n) {
-        if (n <= 0)
-            return false;
+	
+	public static boolean isPowerOfTwo(int n) {
+		if (n <= 0)
+			return false;
 //        double ans = (Math.log(n) / Math.log(2));
 //        return Math.abs(ans - Math.round(ans)) < 1e-10;
-        //another solution
-        return ((n & (n - 1)) == 0);
-
-    }
-
-    public static boolean isPowerOfThree(int n) {
-        if (n <= 0)
-            return false;
+		//another solution
+		return ((n & (n - 1)) == 0);
+		
+	}
+	
+	public static boolean isPowerOfTwo_Log(int n) {
+		if (n <= 0)
+			return false;
+		if (n == 1)
+			return true;
+		double ans = Math.log(n) / Math.log(2);
+        System.out.println(ans);
+        double x=1e-10;
+        return Math.abs(ans - Math.round(ans)) < x;
+	}
+	
+	public static boolean isPowerOfThree(int n) {
+		if (n <= 0)
+			return false;
 //        while (n != 1) {
 //            if (n % 3 != 0) {
 //                return false;
@@ -22,31 +33,32 @@ public class lc_231 {
 //            n = n / 3;
 //        }
 //        return true;
-        double ans = Math.log(n) / Math.log(3);
-        return Math.abs(ans - Math.round(ans)) < 1e-10;
-    }
-
-    //Brute force
-    public static boolean isPowerOfFour_BruteForce(int n) {
-        if (n <= 0)
-            return false;
-        if (n == 1)
-            return true;
-
-        int i = n;
-        while (i != 1) {
-            if (i % 2 != 0) {
-                return false;
-            }
-            i /= 2;
-        }
-        return true;
-    }
-
-
-    public static void main(String[] args) {
-        int n = 3;
-        boolean ans = isPowerOfFour_BruteForce(n);
-        System.out.println(ans);
-    }
+		double ans = Math.log(n) / Math.log(3);
+		System.out.println(ans);
+		return Math.abs(ans - Math.round(ans)) < 1e-10;
+	}
+	
+	//Brute force
+	public static boolean isPowerOfFour_BruteForce(int n) {
+		if (n <= 0)
+			return false;
+		if (n == 1)
+			return true;
+		
+		int i = n;
+		while (i != 1) {
+			if (i % 2 != 0) {
+				return false;
+			}
+			i /= 2;
+		}
+		return true;
+	}
+	
+	
+	public static void main(String[] args) {
+		int n = 536870912;
+		boolean ans = isPowerOfTwo_Log(n);
+		System.out.println(ans);
+	}
 }
