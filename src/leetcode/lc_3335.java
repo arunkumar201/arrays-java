5c57d5bec0cca28e99b85aa496bb4e54b75897c3
Arun Kumar <arunprajapati9825@gmail.com>
2025-05-14 08:12:25 +0530

    updated lc 3335

--- src/leetcode/lc_3335.java
+++ src/leetcode/lc_3335.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class lc_3335 {
@@ -12,6 +13,8 @@ public class lc_3335 {
 		int t = 7517;
 		lc_3335 solution = new lc_3335();
 		System.out.println(solution.lengthAfterTransformations(s, t));
+		System.out.println("--Map solution--");
+		System.out.println(solution.lengthAfterTransformations_mp(s, t));
 	}
 	
 	public int lengthAfterTransformations_bruteForce(String s, int t) {
@@ -44,23 +47,67 @@ public class lc_3335 {
 		
 		int[] cnt = new int[26];
 		
-        for (char ch : s.toCharArray()) {
-            cnt[ch - 'a']++;
-        }
+		for (char ch : s.toCharArray()) {
+			cnt[ch - 'a']++;
+		}
 		
-        for (int round = 0; round < t; round++) {
-            int[] nxt = new int[26];
-            nxt[0] = cnt[25];
-            nxt[1] = (cnt[25] + cnt[0]) % MOD;
-            for (int i = 2; i < 26; ++i) {
-                nxt[i] = cnt[i - 1];
-            }
-            cnt = nxt;
-        }
-        int ans = 0;
-        for (int i = 0; i < 26; ++i) {
-            ans = (ans + cnt[i]) % MOD;
-        }
-        return ans;
-    }
+		for (int round = 0; round < t; round++) {
+			int[] nxt = new int[26];
+			
+			for (int i = 0; i < 26; i++) {
+				//if Z comes, we will replace it with A and B
+				if (i == 25) {
+					nxt[0] = (nxt[0] + cnt[25]) % MOD;
+					nxt[1] = (nxt[1] + cnt[25]) % MOD;
+				} else {
+					// 'a' to 'y' → next character
+					nxt[i + 1] = (nxt[i + 1] + cnt[i]) % MOD;
+				}
+			}
+			cnt = nxt;
+		}
+		int ans = 0;
+		for (int i = 0; i < 26; ++i) {
+			ans = (ans + cnt[i]) % MOD;
+		}
+		return ans;
+	}
+	
+	
+	public int lengthAfterTransformations_mp(String s, int t) {
+		int MOD = 1_000_000_007;
+		
+		HashMap<Character, Integer> map = new HashMap<>();
+		
+		for (char ch : s.toCharArray()) {
+			map.put(ch, map.getOrDefault(ch, 0) + 1);
+		}
+		
+		for (int round = 0; round < t; round++) {
+			HashMap<Character, Integer> nxt = new HashMap<>();
+			for (char ch : map.keySet()) {
+				int curr_char_count = map.getOrDefault(ch, 0);
+				if (ch == 'z') {
+					//if Z comes, we will replace it with A and B
+					int count_a = nxt.getOrDefault('a', 0);
+					int count_b = nxt.getOrDefault('b', 0);
+					
+					nxt.put('a', (count_a + curr_char_count) % MOD);
+					nxt.put('b', (count_b + curr_char_count) % MOD);
+				} else {
+					//will update every character with next character
+					//e.g. if 'a' comes, we will update it with 'b'
+					char nextChar = (char) (ch + 1);
+					int next_char_count = (nxt.getOrDefault(nextChar, 0) + curr_char_count) % MOD;
+					nxt.put(nextChar, next_char_count);
+				}
+			}
+			map = nxt;
+		}
+		int ans = 0;
+		for (int count : map.values()) {
+			ans = (ans + count) % MOD;
+		}
+		return ans;
+	}
 }
