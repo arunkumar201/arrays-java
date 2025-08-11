49bdf637d2471355fc09c536d9caccc48dfa273e
arunp <arunprajapati9825@gmail.com>
2025-08-11 23:04:44 +0530

    added lc 2348

---
+++ src/leetcode/lc_2348.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class lc_2348 {
+	
+	public static void main(String[] args) {
+		//Input: n = 15, queries = [[0,1],[2,2],[0,3]]
+		
+		int n = 13;
+		int[][] queries = new int[][]{{1, 2}, {1, 1}};
+		lc_2348 lc = new lc_2348();
+		int[] res = lc.productQueries(n, queries);
+		System.out.println(Arrays.toString(res));
+	}
+	
+	public int[] calculatePowers(int n, int[][] queries, int[] powers, int k) {
+		int Mod = 1_000_000_007;
+		int[] res = new int[n];
+
+		
+		
+		for (int i = 0; i < n; i++) {
+			int start = queries[i][0];
+			int end = queries[i][1];
+			long ans=1;
+			for (int j = start; j <= end; j++) {
+				ans = (ans * powers[j]);
+			}
+			res[i] = (int) ans % Mod;
+		}
+		return res;
+		
+	}
+	public int[] productQueries(int n, int[][] queries) {
+		int len = queries.length;
+		int[] powers = new int[32];
+		
+		int k = 0;
+		for (int i = 0; i < 32; i++) {
+			if ((n & (1 << i)) != 0) {
+				powers[k++] = 1 << i;
+			}
+		}
+		
+		return calculatePowers(len, queries, powers, k);
+	}
+}
