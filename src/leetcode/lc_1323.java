e5da8919cc71b5f53471688f58344cc64a0b1a4b
arunp <arunprajapati9825@gmail.com>
2025-08-17 19:27:41 +0530

    updated lc 41

--- src/leetcode/lc_1323.java
+++ src/leetcode/lc_1323.java
@@ -1,2 +1,47 @@
-package leetcode;public class lc_1323 {
+package leetcode;
+
+public class lc_1323 {
+	
+	public static void main(String[] args) {
+//		Input: num = 9669
+		//Output: 6996
+		int num = 9669;
+		lc_1323 lc = new lc_1323();
+		int res = lc.maximum69Number(num);
+		System.out.println(res);
+		
+		
+	}
+	
+	public int maximum69Number(int num) {
+		
+		StringBuilder sb = new StringBuilder(Integer.toString(num));
+		int len = sb.length();
+		int target_index = 0;
+		for (int i = 0; i < len; i++) {
+			if (sb.charAt(i) == '6') {
+				target_index = i;
+				break;
+			}
+		}
+		sb.setCharAt(target_index, '9');
+		
+		return Integer.parseInt(sb.toString());
+	}
+	
+	public int maximum69Number_better(int num) {
+		char[] digits = Integer.toString(num).toCharArray();
+		
+		int len = digits.length;
+		int target_index = 0;
+		for (int i = 0; i < len; i++) {
+			if (digits[i] == '6') {
+				target_index = i;
+				break;
+			}
+		}
+		digits[target_index] = '9';
+		return Integer.parseInt(new String(digits));
+	}
 }
+
--- src/leetcode/lc_2787.java
+++ src/leetcode/lc_2787.java
@@ -1,2 +1,18 @@
-package leetcode;public class lc_2787 {
+package leetcode;
+
+public class lc_2787 {
+	public static void main(String[] args) {
+		//Input: n = 10, x = 2
+		
+		int n = 10;
+		int x = 2;
+		lc_2787 lc = new lc_2787();
+		int res = lc.numberOfWays(n, x);
+		System.out.println(res);
+	}
+	
+	public int numberOfWays(int n, int x) {
+		
+		return 0;
+	}
 }
