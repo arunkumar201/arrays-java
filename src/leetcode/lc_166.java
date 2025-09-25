a0c27ae854fb84a3ea62a9ed48c7d078d0e3780c
arun.kumar <arun.kumar@gmail.com>
2025-09-25 08:45:10 +0530

    updated lc 166

--- src/leetcode/lc_166.java
+++ src/leetcode/lc_166.java
@@ -8,14 +8,10 @@ public class lc_166 {
 	public static void main(String[] args) {
 		// Input: numerator = 1, denominator = 2
 		lc_166 sol = new lc_166();
-		System.out.println(sol.fractionToDecimal(1, 2)); // 0.5
-		System.out.println(sol.fractionToDecimal(7, 963)); // "0.(00726895119418483904465212876427829698857736240913811)"
-		System.out.println(sol.fractionToDecimal(2, 1)); // 2
+		System.out.println(sol.fractionToDecimal_optimal(1, 2)); // 0.5
+		System.out.println(sol.fractionToDecimal_optimal(7, 963)); // "0.(00726895119418483904465212876427829698857736240913811)"
+		System.out.println(sol.fractionToDecimal_optimal(1, 6)); // 2
 
-	}
-
-	public String getRepeatingPart(String fraction) {
-		return "";
 	}
 
 	public String fractionToDecimal(int numerator, int denominator) {
@@ -28,7 +24,6 @@ public class lc_166 {
 		if (isDenominatorNegative) {
 			denominator *= -1;
 		}
-		
 
 		int integerPart = numerator / denominator;
 		result.append(integerPart).append('.');
@@ -63,14 +58,78 @@ public class lc_166 {
 			result.deleteCharAt(result.length() - 1);
 		}
 
-		//if both are negative then make it positive else if anyone is negative then make it negative
+		// if both are negative then make it positive else if anyone is negative then
+		// make it negative
 		if (isNegative != isDenominatorNegative) {
 			result.insert(0, '-');
 		}
-		
 
 		return result.toString();
 
+	}
+
+	public String fractionToDecimal_optimal(int numerator, int denominator) {
+		StringBuilder result = new StringBuilder();
+
+		boolean wasNegative = false;
+
+		boolean numerator_was_negative = false;
+		boolean denominator_was_negative = false;
+
+		if (numerator < 0) {
+			numerator_was_negative = true;
+		}
+		if (denominator < 0) {
+			denominator_was_negative = true;
+
+		}
+
+		if (denominator_was_negative != numerator_was_negative) {
+			wasNegative = true;
+		}
+		// now we will append the '-' in start if one of them were negative
+		if (wasNegative && numerator != 0) {
+			result.append("-");
+		}
+
+		// dividend and reminder
+		long l_numerator = Math.abs((long) numerator);
+		long l_denominator = Math.abs((long) denominator);
+
+		long decimalPart = l_numerator / l_denominator;
+
+		if (numerator % denominator == 0) {
+			return result.append(l_numerator / l_denominator).toString();
+		}
+		// add it into the result
+		result.append(Long.toString(decimalPart));
+		// get the reminder
+		long reminder = (l_numerator % l_denominator);
+
+		// append the remainder
+		// before that we should put the "."
+		result.append(".");
+
+		Map<Long, Integer> hasRemainderSeen = new HashMap<>();
+
+		while (reminder != 0) {
+			if (hasRemainderSeen.containsKey(reminder)) {
+				// we found the repeating remainder
+				int repeatingStartIndex = hasRemainderSeen.get(reminder);
+
+				result.insert(repeatingStartIndex, "(");
+				result.insert(result.length(), ")");
+				break;
+			}
+
+			hasRemainderSeen.put(reminder, result.length());
+
+			reminder *= 10;
+			result.append(reminder / l_denominator);
+			reminder = (reminder % l_denominator);
+		}
+
+		return result.toString();
 	}
 
 }
