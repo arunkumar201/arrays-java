54d875966e8f2e343cad5b54542e2681199cd68a
arun.kumar <arun.kumar@gmail.com>
2025-10-17 07:52:11 +0530

    add solutions for lc 2598 and lc 3350

---
+++ src/leetcode/lc_3350.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import java.util.List;
+
+public class lc_3350 {
+	public static void main(String[] args) {
+
+	}
+
+	public int maxIncreasingSubarrays(List<Integer> nums) {
+		int n = nums.size();
+		int max_sized_subarray = 0;
+
+		//Return the maximum possible value of k-max_sized_subarray.
+		for (int i = 0; i < n; i++) {
+			
+		}
+
+		return max_sized_subarray;
+	}
+}
