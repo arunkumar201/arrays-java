8450b7d632993b67f3d02a9f85abc84d93925d5a
arun.kumar <arun.kumar@gmail.com>
2025-11-01 13:37:07 +0530

    feat(leetcode): add optimal solution for lc 3217

--- src/leetcode/lc_3217.java
+++ src/leetcode/lc_3217.java
@@ -1,49 +1,77 @@
 package leetcode;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class lc_3217 {
-	
-	
+
 	public static void main(String[] args) {
-		//Input: nums = [1,2,3], head = [1,2,3,4,5]
-		int[] nums = {1, 2, 3};
+		// Input: nums = [1,2,3], head = [1,2,3,4,5]
+		int[] nums = { 1, 2, 3 };
 		ListNode head = new ListNode(1);
 		head.next = new ListNode(2);
 		head.next.next = new ListNode(3);
 		head.next.next.next = new ListNode(4);
 		head.next.next.next.next = new ListNode(5);
-		
+
 		ListNode res = new lc_3217().modifiedList(nums, head);
 		ListNode.DisplayList(res);
 	}
-	
+
 	public ListNode modifiedList(int[] nums, ListNode head) {
-		HashSet<Integer> set = new HashSet<>();
-		ListNode dummy = new ListNode(0);
-		
+
+		ListNode dummy = new ListNode(-1);
+		// set
+		Set<Integer> set = new HashSet<>();
+
+		ListNode next = head;
+		ListNode curr = dummy;
+
 		for (int num : nums) {
 			set.add(num);
 		}
-		
-		ListNode curr = dummy;
-		ListNode next = head;
-		
+
 		while (next != null) {
-				System.out.println(next.val+curr.val);
-			if (set.contains(next.val)) {
-				next = next.next;
+			if (!set.contains(next.val)) {
 				curr.next = next;
+				curr = curr.next;
+				next = next.next;
 			} else {
-				System.out.println(next.val+"--");
-				curr.next = next;
-				curr=curr.next;
 				next = next.next;
+				curr.next = next;
 			}
 		}
-		
-		
+
 		return dummy.next;
-		
+	}
+
+	public ListNode modifiedList_optimal(int[] nums, ListNode head) {
+		int max = Integer.MIN_VALUE;
+		for (int num : nums) {
+			max = Math.max(max, num);
+		}
+		boolean[] cache = new boolean[max + 1];
+
+		for (int num : nums) {
+			cache[num] = true;
+		}
+
+		ListNode dummy = new ListNode(-1);
+		ListNode next = head;
+		ListNode curr = dummy;
+
+		while (next != null) {
+
+			if (!cache[next.val]) {
+				curr.next = next;
+				curr = curr.next;
+				next = next.next;
+			} else {
+				next = next.next;
+				curr.next = next;
+			}
+
+		}
+		return dummy.next;
 	}
 }
