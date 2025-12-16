626a1b5375bf80f77a8111c6b52a35f0c5d4ae32
arunkumar201 <arunprajapati1091@gmail.com>
2025-12-16 08:37:34 +0530

    feat(sorting): add sorting algorithms

---
+++ src/sorting/InsertionSort.java
@@ -0,0 +1,17 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+	
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5 };
+		InsertionSort sol=new InsertionSort();
+		sol.insertion(arr);
+		System.out.println("Insertion Sort : " + Arrays.toString(arr));
+	}
+
+	public void insertion(int[] arr) {
+		
+	}
+}
---
+++ src/sorting/SelectionSort.java
@@ -0,0 +1,21 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+	
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 3, 4, 5 };
+		SelectionSort sol=new SelectionSort();
+		sol.selectionSort(arr);
+		System.out.println("Selection Sort : " + Arrays.toString(arr));
+	}
+
+	public void selectionSort(int[] arr) {
+		
+
+	}
+	
+
+}
