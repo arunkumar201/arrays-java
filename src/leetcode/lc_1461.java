ef653e8f330c042307d4323352a0a09a523ad913
arunkumar201 <arunprajapati1091@gmail.com>
2026-03-29 08:09:50 +0530

    added lc 2836

---
+++ src/leetcode/lc_1461.java
@@ -0,0 +1,33 @@
+
+package leetcode;
+
+public class lc_1461 {
+
+	
+	public static void main(String[] args) {
+		/// Input: s = "00110110", k = 2
+		
+		String s = "00110110";
+		int k = 2;
+
+		lc_1461 sol = new lc_1461();
+
+		System.out.println("Result "+sol.hasAllCodes(s, k));
+
+	}
+	
+	public boolean hasAllCodes(String s, int k) {
+        //  000
+        //  011
+        //  001
+        //  111
+        //  100
+        //  101
+		//  110
+		
+
+		return false;
+         
+        
+    }
+}
